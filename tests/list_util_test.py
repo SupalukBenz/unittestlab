6b0aed0ac254cc32c633d68acbd162063f6bf8c7
benzsupaluk <supaluk.j@ku.th>
2018-09-07 13:35:00 +0700

    Edited name tests

--- tests/list_util_test.py
+++ tests/list_util_test.py
@@ -3,18 +3,18 @@ from list_util import count_unique
 
 class TestListUtilTest(unittest.TestCase):
 
-    def testListEmpty(self):
+    def test_list_empty(self):
       list = []
       self.assertEqual(0, len(list))
 
-    def testCountUnique(self):
+    def test_count_unique(self):
       list = ['a', 'b', 'a', 'c', 'b']
       self.assertEqual(2, count_unique(list))
       
       list = [1, 2, 3, 1, 5]
       self.assertEqual(1, count_unique(list))
     
-    def testAllSameElement(self):
+    def test_all_same_element(self):
       list = ['a', 'a', 'a', 'a', 'a']
       self.assertEqual(1, count_unique(list))
 
